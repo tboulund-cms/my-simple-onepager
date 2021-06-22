a516369d2ed8315582931a9f22114395e0ca8007
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-06-22 15:47:31 +0200

    Fixed naming

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-function themename_enqueue_style() {
+function mytheme_enqueue_style() {
     wp_enqueue_style('my-style', get_stylesheet_uri() ); 
 }
-add_action('wp_enqueue_scripts', 'themename_enqueue_style');
+add_action('wp_enqueue_scripts', 'mytheme_enqueue_style');
 ?>
